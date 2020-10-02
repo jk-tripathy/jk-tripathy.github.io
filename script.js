21865bc707dd76565bbbf6aa75ad6e76595ada7f
jatinkarthikt@gmail.com <jatinkarthikt@gmail.com>
2020-10-02 20:27:18 +0530

    added responsive research page

--- script.js
+++ script.js
@@ -14,5 +14,28 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true,
     });
-});
 
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 1000,
+        autoplayHoverPause: true,
+        dots: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false,
+            },
+            600:{
+                items: 1,
+                nav: false
+            },
+            1000:{
+                items: 2,
+                nav: false,
+                loop: false,
+            }
+        }
+    });
+});
