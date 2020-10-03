2571e02424f6b3321939cbfbf1c67dd1d781c299
jatinkarthikt@gmail.com <jatinkarthikt@gmail.com>
2020-10-03 11:59:15 +0530

    basic layouting of projects page + js scripting for project type change

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    //toggle ml dl project tab
+    $('.mldl').addClass("active");
+
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar ul').toggleClass("active");
     });
-    $('.navbar ul li a').click(function(){
+    $('.navbar ul li a').click(function () {
         $('.navbar ul').removeClass("active");
     });
 
@@ -23,19 +26,68 @@ $(document).ready(function(){
         autoplayHoverPause: true,
         dots: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false,
             },
-            600:{
+            600: {
                 items: 1,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 2,
                 nav: false,
-                loop: false,
             }
         }
+    });
+
+    //projects tab toggle
+    var mldl = document.getElementById("mldlbody");
+    var appdev = document.getElementById("appdevbody");
+    var webdev = document.getElementById("webdevbody");
+    var misc = document.getElementById("miscbody");
+    $('.mldl').click(function () {
+        mldl.style.display = "flex";
+        appdev.style.display = "none";
+        webdev.style.display = "none";
+        misc.style.display = "none";
+
+        $('.mldl').addClass("active");
+        $('.appdev').removeClass("active");
+        $('.webdev').removeClass("active");
+        $('.miscprojects').removeClass("active");
+    });
+    $('.appdev').click(function () {
+        appdev.style.display = "flex";
+        mldl.style.display = "none";
+        webdev.style.display = "none";
+        misc.style.display = "none";
+
+        $('.appdev').addClass("active");
+        $('.mldl').removeClass("active");
+        $('.webdev').removeClass("active");
+        $('.miscprojects').removeClass("active");
+    });
+    $('.webdev').click(function () {
+        webdev.style.display = "flex";
+        appdev.style.display = "none";
+        mldl.style.display = "none";
+        misc.style.display = "none";
+
+        $('.webdev').addClass("active");
+        $('.appdev').removeClass("active");
+        $('.mldl').removeClass("active");
+        $('.miscprojects').removeClass("active");
+    });
+    $('.miscprojects').click(function () {
+        misc.style.display = "flex";
+        appdev.style.display = "none";
+        webdev.style.display = "none";
+        mldl.style.display = "none";
+
+        $('.miscprojects').addClass("active");
+        $('.appdev').removeClass("active");
+        $('.webdev').removeClass("active");
+        $('.mldl').removeClass("active");
     });
 });
