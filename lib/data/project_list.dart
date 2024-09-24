3fc562aa03931681093496f987ba264348b93500
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-09-24 07:18:49 +0200

    chore: rename file

---
+++ lib/data/project_list.dart
@@ -0,0 +1,58 @@
+const List<Map<String, dynamic>> project_list = [
+  {
+    'title': 'AbsArgSumm',
+    'focus': [
+      'Text Generation',
+      'Argument Mining',
+      'Abstractive Summarization',
+    ],
+    'link': 'https://github.com/jk-tripathy/absargsumm',
+    'content': [
+      'Abstractive summarization of large scientific documents with an average of 7000 tokens.',
+      'Novel approach of using Argument Structure to improve the summarization.',
+      'Engineered a custom abstractive summarization model, which improved summary coherence by 3% based on ROUGE metrics.',
+    ]
+  },
+  {
+    'title': 'XAI: Prompting and Evaluation of Multimodal QA',
+    'focus': [
+      'Prompting LLMs',
+      'Text Generation',
+      'Explainable AI',
+    ],
+    'link': 'https://github.com/jk-tripathy/Prompting-and-Rationale-Generation',
+    'content': [
+      'Exploring different methods of prompting 3 different Large Language Models.',
+      'Representing an image context in 6 different textual forms for Retrieval Augmented Generation',
+      'Question Answering and Rationale Generation bench-marking on 2 different datasets.',
+    ]
+  },
+  {
+    'title': 'Sexism Detection using MoE',
+    'focus': [
+      'Text Classification',
+      'Sentiment Analysis',
+      'Mixture of Experts',
+    ],
+    'link': 'https://github.com/jk-tripathy/Explainable-Sexism',
+    'content': [
+      'Multiclass Classification of 4 types of Online Sexism.',
+      'Engineered a custom granular sexism classification method built using a Master of Experts architecture.',
+      'Obtained an macro F1 score of 0.5288, matching state-of-the-art.',
+    ]
+  },
+  {
+    'title': 'Knowledge Enhancement for PCL',
+    'focus': [
+      'Text Classification',
+      'Sentiment Analysis',
+      'Knowledge Graphs',
+    ],
+    'link': 'https://github.com/jk-tripathy/Knowledge-Enchancement-for-PCL',
+    'content': [
+      'Multiclass Classification of 7 types of Patronizing and Condescending Language commonly found online.',
+      'Performed Statistical Analysis of the data to provide more insights.',
+      ' Implemented fine-tuning of a knowledge graph embedded LLM, obtaining a micro F1 score of 0.701, beating state-of-the-art.',
+    ]
+  },
+];
