3866341bb6f630fe84b67fd8d5c517bc1bfbee47
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-09-24 15:21:36 +0200

    feat: papers page done

---
+++ lib/data/paper_list.dart
@@ -0,0 +1,39 @@
+const List<Map<String, String>> papers_list = [
+  {
+    'title': 'e-Inu: Simulating A Quadruped Robot With Emotional Sentience.',
+    'authors': 'Chakravarty, A. et al.',
+    'link': 'https://arxiv.org/abs/2301.00964',
+    'abstract':
+        'This paper discusses the understanding and virtual simulation of such a robot capable of detecting and understanding human emotions, generating its gait, and responding via sounds and expression on a screen. To this end, we use a combination of reinforcement learning and software engineering concepts to simulate a quadruped robot that can understand emotions, navigate through various terrains and detect sound sources, and respond to emotions using audio-visual feedback. This paper aims to establish the framework of simulating a quadruped robot that is emotionally intelligent and can primarily respond to audio-visual stimuli using motor or audio response. The emotion detection from the speech was not as performant as ERANNs or Zeta Policy learning, still managing an accuracy of 63.5%. The video emotion detection system produced results that are almost at par with the state of the art, with an accuracy of 99.66%. Due to its "on-policy" learning process, the PPO algorithm was extremely rapid to learn, allowing the simulated dog to demonstrate a remarkably seamless gait across the different cadences and variations. This enabled the quadruped robot to respond to generated stimuli, allowing us to conclude that it functions as predicted and satisfies the aim of this work.',
+  },
+  {
+    'title': 'HRV stress analysis using k-NN clustering technique.',
+    'authors': 'Tripathy, J. K. et al.',
+    'link':
+        'https://pubs.aip.org/aip/acp/article-abstract/2579/1/020033/2915365/HRV-stress-analysis-using-k-NN-clustering',
+    'abstract':
+        'Human life in the modern scenario has become quite taxing; mental health issues are on the rise. Our medical science may have progressed well enough to furnish physical health excellency in the general populace (of course, in relative terms), but mental health is still taboo. This is especially so in many of the non-first-world countries, where forthcoming mental health issues and asking for help can lead to anything from public ridicule to social ostracism. In such a scenario, it is of paramount importance that someone comes up with a more private approach to mental health care and well-being. In this paper, the authors discuss how the effects of stress can be monitored using a different implementation of k-NN (k-Nearest Neighbors). With this, using the Manhattan distance metric we were able to achieve 99.84% accuracy, and even after the application of PCA, we achieved 93.97% accuracy.',
+  },
+  {
+    'title': 'Comprehensive analysis of embeddings and pre-training in NLP.',
+    'authors': 'Tripathy, J. K. et al.',
+    'link':
+        'https://www.sciencedirect.com/science/article/abs/pii/S1574013721000733',
+    'abstract':
+        'The amount of data and computing power has drastically increased over the last decade, which leads to the development of several new fronts in the field of Natural Language Processing (NLP). In addition to that, the entanglement of embeddings and large pre-trained models have pushed the field forward, covering a wide variety of tasks starting from machine translation to more complex tasks such as contextual text classification. This paper covers the underlying idea behind all embeddings and pre-trained models and provides an insight into fundamental strategies and implementation details of innovative embeddings. Further, it imparts the pros and cons of each specific embedding design and the associated impact on the result. It also comprehends the comparison of all the different strategies, datasets, architectures discussed in different papers with the help of standard metrics used in NLP. The content covered in this review work aims to shed light on different milestones reached in NLP, allowing the reader to deepen their understanding of NLP, which would motivate to explore the field further.',
+  },
+  {
+    'title': 'ALBERT-based fine-tuning model for cyberbullying analysis.',
+    'authors': 'Tripathy, J. K. et al.',
+    'link': 'https://link.springer.com/article/10.1007/s00530-020-00690-5',
+    'abstract':
+        'This paper explores some of the recent approaches to the difficulties of contextual understanding and proposes an ALBERT-based fine-tuned model that achieves state-of-the-art results. ALBERT is a transformer-based architecture and thus even at its untrained form provides better contextual understanding than other recurrent units. This coupled with the fact that ALBERT is pre-trained on a large corpus allowing the flexibility to use a smaller dataset for fine-tuning as the pre-trained model already has deep understanding of the complexities of the human language. ALBERT showcases high scores in multiple benchmarks such as the GLUE and SQuAD showing that high levels of contextual understanding are inherently present and thus fine-tuning for the specific case of cyberbullying allows to use this to our advantage. With this approach, we have achieved an F1 score of 95% which surpasses current approaches such as the CNN + wordVec, CNN + GRU and BERT implementations.',
+  },
+  {
+    'title': 'Pattern Prediction using LSTM and ReLU Ensemble.',
+    'authors': 'Chakravarty, A. & Tripathy, J. K.',
+    'link': 'https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3569584',
+    'abstract':
+        'Human beings, like many other cognizant biological organisms, can roughly predict certain patterns in nature, be it with respect to time, or otherwise. In the strive to achieve the global object of achieving a general artificial intelligence, the authors deem it to be of high importance that the intelligence is aware of its surroundings, not just in terms of understanding raw sensory data, but also by learning and being able to predict recurring patterns in the environment. To achieve that result, this paper comes into play by discussing certain neural network models which help in learning from any sort of pattern recognition in a data sequence which can be achieved by either manual input or via data collection from the outer environment. This model is able to predict accurately about 80% of the time, beating state-of-the-art approaches.',
+  },
+];
--- lib/screens/papers.dart
+++ lib/screens/papers.dart
@@ -1,7 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:portfolio/data/dimensions.dart';
-import 'package:portfolio/data/project_list.dart';
+import 'package:portfolio/data/paper_list.dart';
 import 'package:portfolio/widgets/custom_hero.dart';
+import 'package:portfolio/widgets/link_button.dart';
 import 'package:portfolio/widgets/page_indicator.dart';
 import 'package:portfolio/widgets/top_bar.dart';
 import 'package:flutter/foundation.dart';
@@ -23,7 +26,7 @@ class _PapersViewState extends State<PapersView> with TickerProviderStateMixin {
   void initState() {
     super.initState();
     _pageViewController = PageController();
-    _tabController = TabController(length: project_list.length, vsync: this);
+    _tabController = TabController(length: papers_list.length, vsync: this);
   }
 
   @override
@@ -31,6 +34,41 @@ class _PapersViewState extends State<PapersView> with TickerProviderStateMixin {
     super.dispose();
     _pageViewController.dispose();
     _tabController.dispose();
+  }
+
+  void _handlePageViewChanged(int currentPageIndex) {
+    if (!_isOnDesktopAndWeb) {
+      return;
+    }
+    _tabController.index = currentPageIndex;
+    setState(() {
+      _currentPageIndex = currentPageIndex;
+    });
+  }
+
+  void _updateCurrentPageIndex(int index) {
+    _tabController.index = index;
+    _pageViewController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 400),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  bool get _isOnDesktopAndWeb {
+    if (kIsWeb) {
+      return true;
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.macOS:
+      case TargetPlatform.linux:
+      case TargetPlatform.windows:
+        return true;
+      case TargetPlatform.android:
+      case TargetPlatform.iOS:
+      case TargetPlatform.fuchsia:
+        return false;
+    }
   }
 
   @override
@@ -78,7 +116,7 @@ class _PapersViewState extends State<PapersView> with TickerProviderStateMixin {
                           child: PageView(
                             controller: _pageViewController,
                             onPageChanged: _handlePageViewChanged,
-                            children: PageViewContent(context),
+                            children: PageViewContent(context, constraints),
                           ),
                         ),
                       ],
@@ -99,46 +137,85 @@ class _PapersViewState extends State<PapersView> with TickerProviderStateMixin {
     );
   }
 
-  List<Widget> PageViewContent(BuildContext context) {
-    return project_list.map((project) {
+  List<Widget> PageViewContent(
+      BuildContext context, BoxConstraints constraints) {
+    return papers_list.map((paper) {
       return Center(
-        child: Text(project['title']!),
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          paper['title']!,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall!
+                              .copyWith(
+                                color: Theme.of(context).primaryColorLight,
+                              ),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              'by ',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelLarge!
+                                  .copyWith(
+                                    color: Theme.of(context).hintColor,
+                                  ),
+                            ),
+                            Text(
+                              paper['authors']!,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .labelLarge!
+                                  .copyWith(
+                                    color: Theme.of(context).primaryColorLight,
+                                  ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  LinkButton(
+                    url: paper['link']!,
+                    img: 'assets/img/paper.png',
+                    imgDim: min(
+                      MediaQuery.of(context).size.width * 0.07,
+                      MediaQuery.of(context).size.height * 0.06,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: constraints.maxWidth < mobileWidth
+                    ? MediaQuery.of(context).size.height * 0.45
+                    : MediaQuery.of(context).size.height * 0.40,
+                child: SingleChildScrollView(
+                  child: Text(
+                    paper['abstract']!,
+                    style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                          color: Theme.of(context).primaryColorLight,
+                          height: 1.5,
+                        ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       );
     }).toList();
-  }
-
-  void _handlePageViewChanged(int currentPageIndex) {
-    if (!_isOnDesktopAndWeb) {
-      return;
-    }
-    _tabController.index = currentPageIndex;
-    setState(() {
-      _currentPageIndex = currentPageIndex;
-    });
-  }
-
-  void _updateCurrentPageIndex(int index) {
-    _tabController.index = index;
-    _pageViewController.animateToPage(
-      index,
-      duration: const Duration(milliseconds: 400),
-      curve: Curves.easeInOut,
-    );
-  }
-
-  bool get _isOnDesktopAndWeb {
-    if (kIsWeb) {
-      return true;
-    }
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.macOS:
-      case TargetPlatform.linux:
-      case TargetPlatform.windows:
-        return true;
-      case TargetPlatform.android:
-      case TargetPlatform.iOS:
-      case TargetPlatform.fuchsia:
-        return false;
-    }
   }
 }
