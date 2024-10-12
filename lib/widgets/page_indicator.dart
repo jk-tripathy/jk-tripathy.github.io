44de1856a4441906a37b962075f08832b962da10
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2024-10-12 13:44:31 +0200

    fix: weird tab controller end pos bug

--- lib/widgets/page_indicator.dart
+++ lib/widgets/page_indicator.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class PageIndicator extends StatelessWidget {
@@ -28,20 +30,25 @@ class PageIndicator extends StatelessWidget {
             splashRadius: 16.0,
             padding: EdgeInsets.zero,
             onPressed: () {
-              if (currentPageIndex == 0) {
-                onUpdateCurrentPageIndex(tabController.length - 1);
+              if (currentPageIndex != 0) {
+                onUpdateCurrentPageIndex(currentPageIndex - 1);
               }
-              onUpdateCurrentPageIndex(currentPageIndex - 1);
             },
             icon: Icon(
               Icons.arrow_left_rounded,
-              size: 64.0,
+              size: min(
+                MediaQuery.of(context).size.width * 0.1,
+                MediaQuery.of(context).size.height * 0.065,
+              ),
               color: Theme.of(context).hintColor,
             ),
           ),
           TabPageSelector(
             controller: tabController,
-            indicatorSize: 16.0,
+            indicatorSize: min(
+              MediaQuery.of(context).size.width * 0.1 / 4,
+              MediaQuery.of(context).size.height * 0.065 / 4,
+            ),
             color: Theme.of(context).primaryColorLight,
             selectedColor: Theme.of(context).hintColor,
             borderStyle: BorderStyle.none,
@@ -50,14 +57,16 @@ class PageIndicator extends StatelessWidget {
             splashRadius: 16.0,
             padding: EdgeInsets.zero,
             onPressed: () {
-              if (currentPageIndex == tabController.length - 1) {
-                onUpdateCurrentPageIndex(0);
+              if (currentPageIndex != tabController.length - 1) {
+                onUpdateCurrentPageIndex(currentPageIndex + 1);
               }
-              onUpdateCurrentPageIndex(currentPageIndex + 1);
             },
             icon: Icon(
               Icons.arrow_right_rounded,
-              size: 64.0,
+              size: min(
+                MediaQuery.of(context).size.width * 0.1,
+                MediaQuery.of(context).size.height * 0.065,
+              ),
               color: Theme.of(context).hintColor,
             ),
           ),
